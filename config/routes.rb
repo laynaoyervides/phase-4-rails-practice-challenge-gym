8620a8255bdf1ed08c8a219eb8daaf67c5d91ae5
Layna Oyervides <laynaoyervides@gmail.com>
2022-08-05 09:03:01 -0600

    halfway done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :clients
-  resources :gyms
-  resources :memberships
+  resources :clients, only [:show, :index, :update]
+  resources :gyms, only: [:show, :delete,]
+  resources :memberships, only: [:create, :delete,]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
